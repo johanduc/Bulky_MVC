6dced6638b48fa44c61ab2f2b3eecea332e11df2
Duc Nguyen <nguyenxuanduc94@gmail.com>
2025-08-29 15:12:16 +0700

    Commit 2025-08-29: ViewBag, ViewData and TempData

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,5 +40,11 @@ namespace BulkyBook.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int CategoryID { get; set; }
+        [ForeignKey("CategoryID")]
+        public Category Category { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
